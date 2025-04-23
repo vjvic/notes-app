6883600c4fc96444b28ff0f477e5cce5b864ccc4
vjvic <victorsulit0@gmail.com>
2025-04-23 13:34:37 +0800

    feat/create searchbar ui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,18 @@
 <template>
   <!-- Header -->
-  <header class="header">
+  <header class="header uk-flex uk-flex-between">
     <div class="header__title">
-      <h2 class="uk-text-bold uk-margin-remove">All Notes</h2>
+      <h3 class="uk-text-bold uk-margin-remove">All Notes</h3>
     </div>
-    <div class="header__user">
-      <span>User Profile</span>
+
+    <div class="header__settings uk-flex uk-width-1-6">
+      <SearchBar />
+      <Settings />
     </div>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Settings } from "lucide-vue-next";
+import SearchBar from "./SearchBar.vue";
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <form class="uk-search uk-search-default uk-margin-small-right uk-width-1-1">
+    <span uk-search-icon></span>
+    <input
+      class="uk-search-input uk-border-rounded"
+      type="search"
+      placeholder="search by title, content or tags..."
+      aria-label="Search"
+    />
+  </form>
+</template>
